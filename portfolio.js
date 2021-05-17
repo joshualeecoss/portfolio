df78bd0d2f5eba3f67751d06cf5f1cb7058f2073
Josh Coss <cossj3@mymacewan.ca>
2021-05-16 21:03:11 -0600

    first commit

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+// toggle collapse of specified content
+function toggleContent(content) {
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  }
+  
+  // collapse all open content
+  function collapseAllOpenContent() {
+    const colls = document.getElementsByClassName('collapsible');
+    for (const coll of colls) {
+      if (coll.classList.contains('active')) {
+        coll.classList.remove('active');
+        toggleContent(coll.nextElementSibling);
+      }
+    }
+  }
+  
+  const colls = document.getElementsByClassName('collapsible');
+  for (const coll of colls) {
+    coll.addEventListener('click', function() {
+      if (!this.classList.contains('active')) {
+        collapseAllOpenContent();
+      }
+      this.classList.toggle('active');
+      toggleContent(this.nextElementSibling);
+    });
+  }
